b465e1bf19f7c40eb0c5a6afeaaa016454efa91f
amirradjou <44401968+amirradjou@users.noreply.github.com>
2020-08-17 11:00:23 +0430

    Update ai.py

--- PythonRandomClient/ai.py
+++ PythonRandomClient/ai.py
@@ -4,6 +4,9 @@
 import random
 
 # chillin imports
+import time
+from collections import Counter
+
 from chillin_client import RealtimeAI
 
 # project imports
@@ -18,10 +21,8 @@ class AI(RealtimeAI):
         super(AI, self).__init__(world)
         self.stage = 0
 
-
     def initialize(self):
         print('initialize')
-
 
     def decide(self):
         print('decide')
@@ -31,11 +32,22 @@ class AI(RealtimeAI):
         wagent = base.agents[AgentType.Warehouse]
         fagent = base.agents[AgentType.Factory]
 
+        # Inja dictionary haro mirizam too moteghayerash
+        goldenTank = Counter(self.world.bases[self.my_side].factory.c_mixture_formulas[AmmoType.GoldenTankShell])
+        rifleBullet = Counter(self.world.bases[self.my_side].factory.c_mixture_formulas[AmmoType.RifleBullet])
+        mortarShell = Counter(self.world.bases[self.my_side].factory.c_mixture_formulas[AmmoType.MortarShell])
+        hmgBullet = Counter(self.world.bases[self.my_side].factory.c_mixture_formulas[AmmoType.HMGBullet])
+        tankShell = Counter(self.world.bases[self.my_side].factory.c_mixture_formulas[AmmoType.TankShell])
+
+        # inja jam mizanam ke jolo tar azash estefade konim
+        required_materials = goldenTank + tankShell
+
         if self.stage == 0:
             self.warehouse_agent_move(forward=True)
             if base.c_area[wagent.position] == ECell.Material:
                 material_type = base.warehouse.materials[wagent.position].type
-                if wagent.materials_bag[material_type] == 0:
+                if material_type in required_materials and wagent.materials_bag[material_type] != required_materials[
+                    material_type]:
                     self.warehouse_agent_pick_material()
             elif base.c_area[wagent.position] == ECell.BacklineDelivery:
                 self.warehouse_agent_put_material()
@@ -43,43 +55,61 @@ class AI(RealtimeAI):
 
         elif self.stage == 1:
             if base.c_area[fagent.position] == ECell.BacklineDelivery:
-                required_materials = self.world.bases[self.my_side].factory.c_mixture_formulas[AmmoType.RifleBullet]
                 self.factory_agent_pick_material(materials=required_materials)
                 self.stage += 1
             else:
                 self.factory_agent_move(forward=False)
 
         elif self.stage == 2:
-            if (    base.c_area[fagent.position] == ECell.Machine and
+            if (base.c_area[fagent.position] == ECell.Machine and
                     base.factory.machines[fagent.position].status == MachineStatus.Idle):
-                self.factory_agent_put_material(desired_ammo=AmmoType.RifleBullet)
-                self.stage += 1
+                self.factory_agent_put_material(desired_ammo=AmmoType.TankShell)
+                self.stage = 98
             else:
                 self.factory_agent_move(forward=True)
 
-        elif self.stage == 3:
-            if base.factory.machines[fagent.position].status == MachineStatus.AmmoReady:
-                self.factory_agent_pick_ammo()
-                self.stage += 1
+        elif self.stage == 98:
+            if (base.c_area[fagent.position] == ECell.Machine and
+                    base.factory.machines[fagent.position].status == MachineStatus.Idle):
+                self.factory_agent_put_material(desired_ammo=AmmoType.GoldenTankShell)
+                self.stage = 97
+            else:
+                self.factory_agent_move(forward=True)
 
-        elif self.stage == 4:
-            if base.c_area[fagent.position] == ECell.BacklineDelivery:
-                self.factory_agent_put_ammo()
-                self.stage += 1
+        elif self.stage == 97:
+            self.warehouse_agent_move(forward=True)
+            if base.c_area[wagent.position] == ECell.Material:
+                material_type = base.warehouse.materials[wagent.position].type
+                if material_type in required_materials and wagent.materials_bag[material_type] != required_materials[
+                    material_type]:
+                    self.warehouse_agent_pick_material()
+            elif base.c_area[wagent.position] == ECell.BacklineDelivery:
+                self.warehouse_agent_put_material()
+
+            if base.c_area[fagent.position] == ECell.Machine and base.factory.machines[
+                fagent.position].status != MachineStatus.Idle:
+                if base.factory.machines[fagent.position].status == MachineStatus.AmmoReady:
+                    self.factory_agent_pick_ammo()
             else:
                 self.factory_agent_move(forward=False)
+            if base.c_area[fagent.position] == ECell.BacklineDelivery:
+                self.factory_agent_put_ammo()
 
-        elif self.stage == 5:
-            self.warehouse_agent_pick_ammo(ammos={AmmoType.RifleBullet: 1})
-            self.stage += 1
+            if base.c_area[wagent.position] == ECell.BacklineDelivery and base.c_area[
+                fagent.position] == ECell.BacklineDelivery:
+                self.stage = 96
 
-        elif self.stage == 6:
+        elif self.stage == 96:
+            if base.c_area[wagent.position] == ECell.BacklineDelivery:
+                self.warehouse_agent_pick_ammo(ammos={AmmoType.GoldenTankShell: 1, AmmoType.TankShell: 1})
+                self.stage = 95
+
+        elif self.stage == 95:
             if base.c_area[wagent.position] == ECell.FrontlineDelivery:
                 self.warehouse_agent_put_ammo()
                 self.stage = 0
             else:
                 self.warehouse_agent_move(forward=False)
-
 
     # Warehouse Agent Commands
 
